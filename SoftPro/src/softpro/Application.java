8b82e8f0e664001162dff4b57753dc1e19759ee3
Mario Alberto Segura Oliva <marioalbertoseguraoliva@gmail.com>
2016-11-24 23:56:35 +0000

    Develop Updated

--- SoftPro/src/softpro/Application.java
+++ SoftPro/src/softpro/Application.java
@@ -14,29 +14,31 @@ public class Application {
     private void start() {
                     
             SqliteInterface sqliteInterface = new SqliteInterface();
+            /*Map<String, Object> mapaValores = new HashMap<>();
             
-            Map<String, Object> mapaValores = new HashMap<>();
             mapaValores.put("id", null);
             mapaValores.put("telefono", 000000165);
             mapaValores.put("mail", "00001@gmail.com");
             mapaValores.put("nombre", "Donald Duck");
             sqliteInterface.insertInto("staff", mapaValores);
             
-            
+            */
             String[] myStringArray = {"*"};
             List<String> result = sqliteInterface.selectFrom("staff", myStringArray);
             
-            for (String string : result) {
+            result.stream().forEach((string) -> {
                 System.out.println(string);
-            }
-     //       sqliteInterface.deleteFrom("staff", "id = 4");
- //           Map<String, Object> mapaValores = new HashMap<>();
-   //         mapaValores.put("nombre", "HDP");
-            
-     //       sqliteInterface.update("staff", mapaValores, "nombre = 'Donald'");
-            
-       //     result = sqliteInterface.selectFrom("staff", myStringArray);
-         //   for (String string : result) System.out.println(string);
+            });
+
+            /*sqliteInterface.deleteFrom("staff", "id = 4");
+            Map<String, Object> mapaValores = new HashMap<>();
+            mapaValores.put("nombre", "HDP");
+            sqliteInterface.update("staff", mapaValores, "nombre = 'Donald'");
+            result = sqliteInterface.selectFrom("staff", myStringArray);
+
+            for (String string : result)
+            System.out.println(string);
+            */
     }
 
 }
