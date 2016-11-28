f944c5f713f55e854a826f4acedc2be2e2646039
AbianSM <abin473@gmail.com>
2016-11-28 00:17:11 +0000

    Team.java y user.java

--- SoftPro/src/softpro/Model/Team.java
+++ SoftPro/src/softpro/Model/Team.java
@@ -1,6 +1,29 @@
 
 package softpro.Model;
 
-public class Team {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
 
+public class Team implements Iterable<User> {
+    private final List<User> staff;
+    private final int idProject;
+    
+    public Team(int project){
+        this.idProject = project;
+        this.staff = new ArrayList<>();
+    }
+    
+    public void addUser(User user){
+        this.staff.add(user);
+    }
+    
+    public void removeUser(User user){
+        this.staff.remove(user);
+    }
+    
+    @Override
+    public Iterator<User> iterator() {
+        return this.staff.iterator();
+    }
 }
