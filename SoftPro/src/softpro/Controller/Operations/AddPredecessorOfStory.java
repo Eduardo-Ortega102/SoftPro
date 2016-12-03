cfa88b96f465867043f2bd3ccb9ed7dddfedf11e
Eduardo Ortega <eduardo.ortega1002@gmail.com>
2016-12-03 16:48:47 +0000

    AddPredecessorOfStory, first implementation

--- SoftPro/src/softpro/Controller/Operations/AddPredecessorOfStory.java
+++ SoftPro/src/softpro/Controller/Operations/AddPredecessorOfStory.java
@@ -6,18 +6,31 @@
 package softpro.Controller.Operations;
 
 import java.util.HashMap;
+import java.util.Map;
 import softpro.Model.Scrum.ScrumProject;
+import softpro.Model.Scrum.UserStory;
+import softpro.Persistence.Database.BDInterface;
+import softpro.Persistence.Database.SqliteInterface;
 import softpro.View.ActionOverProject;
+import static java.lang.Integer.valueOf;
 
-/**
- *
- * @author Mictlan
- */
 public class AddPredecessorOfStory implements ActionOverProject<ScrumProject> {
 
     @Override
-    public boolean execute(ScrumProject project, HashMap arguments) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean execute(ScrumProject project, HashMap<String, String> arguments) {
+        UserStory story = project.getBacklog().findStory(valueOf(arguments.get("storyID")));
+        UserStory predecessor = project.getBacklog().findStory(valueOf(arguments.get("predecessorID")));
+        return story == null || predecessor == null ? false : 
+               databaseInsertionError(story, predecessor) ? false:
+               story.addPredecessor(predecessor);
+    }
+
+    private boolean databaseInsertionError(UserStory story, UserStory predecessor) {
+        BDInterface sqliteInterface = new SqliteInterface();
+        Map<String, Object> mapa = new HashMap<>();
+        mapa.put("story", story.getId());
+        mapa.put("predecessor", predecessor.getId());
+        return sqliteInterface.insertInto("story_predecessors", mapa);
     }
     
 }
