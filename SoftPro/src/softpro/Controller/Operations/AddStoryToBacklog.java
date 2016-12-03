62eebd6eb7297e09ae58e89408d77d96ac853856
Eduardo Ortega <eduardo.ortega1002@gmail.com>
2016-12-03 23:17:14 +0000

    AddStoryToBacklog, first implementation

--- SoftPro/src/softpro/Controller/Operations/AddStoryToBacklog.java
+++ SoftPro/src/softpro/Controller/Operations/AddStoryToBacklog.java
@@ -1,26 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package softpro.Controller.Operations;
 
 import java.util.HashMap;
+import java.util.Map;
 import softpro.Model.Scrum.ScrumProject;
+import softpro.Model.Scrum.UserStory;
+import softpro.Persistence.Database.SqliteInterface;
+import static softpro.Persistence.IdGenerator.generateNewIdForTable;
 import softpro.View.ActionOverProject;
 
-/**
- *
- * @author Mictlan
- */
 public class AddStoryToBacklog implements ActionOverProject<ScrumProject> {
-
-    public AddStoryToBacklog() {
-    }
 
     @Override
     public boolean execute(ScrumProject project, HashMap<String, String> arguments) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (UserStory userStory : project.getBacklog())
+            if (userStory.getDescription().equalsIgnoreCase(arguments.get("description"))) return false;
+        UserStory story = project.getBacklog().create(generateNewIdForTable("features"), arguments.get("description"));
+        return storeInDatabase(story, project);
     }
-    
+
+    private boolean storeInDatabase(UserStory story, ScrumProject project) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", story.getId());
+        map.put("project", project.getId());
+        map.put("type", "UserStory");
+        map.put("description", story.getDescription());
+        map.put("details", story.getDetails());
+        map.put("points", story.getPoints());
+        map.put("priority", story.getPriority());
+        map.put("responsible", story.getResponsible().getId());
+        map.put("state", story.getState().toString());
+        return new SqliteInterface().insertInto("features", map);
+    }
 }
--- SoftPro/src/softpro/Persistence/IdGenerator.java
+++ SoftPro/src/softpro/Persistence/IdGenerator.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import softpro.Persistence.Database.SqliteInterface;
 import static java.lang.Integer.valueOf;
-import static java.lang.Integer.valueOf;
 
 public class IdGenerator {
 
