8ce769a28c37565f84ba475b28ca32f2737f887a
Abian <Abian@192.168.1.21>
2016-12-04 13:06:22 +0000

    RemoveStoryFromSprint.java

--- SoftPro/src/softpro/Controller/Operations/RemoveStoryFromSprint.java
+++ SoftPro/src/softpro/Controller/Operations/RemoveStoryFromSprint.java
@@ -1,26 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package softpro.Controller.Operations;
 
+import static java.lang.Integer.valueOf;
 import java.util.HashMap;
 import softpro.Model.Scrum.ScrumProject;
+import softpro.Model.Scrum.Sprint;
+import softpro.Model.Scrum.UserStory;
+import softpro.Persistence.Database.SqliteInterface;
 import softpro.View.ActionOverProject;
 
-/**
- *
- * @author Mictlan
- */
 public class RemoveStoryFromSprint implements ActionOverProject<ScrumProject> {
-
-    public RemoveStoryFromSprint() {
-    }
 
     @Override
     public boolean execute(ScrumProject project, HashMap<String, String> arguments) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Sprint sprint = project.findSprint(valueOf(arguments.get("sprintID")));
+        UserStory story = project.getBacklog().findStory(valueOf(arguments.get("storyID")));
+        if(sprint == null) return false;
+        if(story == null) return false;
+        if(databaseDeleteStoryFromSprintError(sprint.getId(),story.getId())==false) return false;
+        return sprint.removeUserStory(story);
     }
     
+    private boolean databaseDeleteStoryFromSprintError(int sprintID, int storyID){
+        String row = "story = "+storyID+ " AND sprint = " + sprintID;
+        return new SqliteInterface().deleteFrom("sprints",row);
+    }
 }
