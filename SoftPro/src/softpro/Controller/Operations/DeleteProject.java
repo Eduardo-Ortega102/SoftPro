ee3546979a66085645c27eae5cdea4498b4b3ec7
Abian <Abian@10.230.175.219>
2016-12-07 11:54:00 +0000

    DeleteProject.java, implementado y testeado

--- SoftPro/src/softpro/Controller/Operations/DeleteProject.java
+++ SoftPro/src/softpro/Controller/Operations/DeleteProject.java
@@ -1,25 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package softpro.Controller.Operations;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import softpro.Controller.CommandSet;
+import softpro.Model.Scrum.ScrumProject;
+import softpro.Model.Scrum.Sprint;
+import softpro.Model.Scrum.UserStory;
+import softpro.Persistence.Database.SqliteInterface;
+import softpro.Persistence.ProjectLoader;
+import softpro.View.ActionSet;
 import softpro.View.AdministrativeAction;
+import softpro.View.Operation;
 
-/**
- *
- * @author Mictlan
- */
 public class DeleteProject implements AdministrativeAction {
 
-    public DeleteProject() {
-    }
-
+    private static final SqliteInterface sqliteInterface = new SqliteInterface();
+    private ActionSet actionSet;
+    
     @Override
     public boolean execute(HashMap<String, String> arguments) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(arguments.get("projectID") == null) return false;
+        String[] select = {"*"};
+        String where = arguments.get("projectID");
+        List<HashMap<String, String>> project = sqliteInterface.selectFrom("projects", select, where);
+        List<String> iDs = new ArrayList<>();
+        for (HashMap<String, String> hashMap : project) {
+            if(hashMap.get("type").equals("Scrum")) iDs.add(""+hashMap.get("id"));
+        }
+        for (String id : iDs) {
+            DeleteScrumProject(id);
+        }
+        return false;
     }
     
+    private boolean DeleteScrumProject(String projectID){
+        ScrumProject scrumProject = ProjectLoader.loadScrumProject(Integer.valueOf(projectID));
+        actionSet = new CommandSet();
+        List<String> iDs = new ArrayList<>();
+        for (Sprint sprint : scrumProject) {
+            iDs.add(""+sprint.getId());
+        }
+        for (String iD : iDs) {
+            HashMap<String, String> arguments = new HashMap<>();
+            arguments.put("sprintID", iD);
+            actionSet.getProjectAction(Operation.DELETE_SPRINT).execute(scrumProject, arguments);
+        }
+        
+        iDs = new ArrayList<>();
+        for (UserStory story : scrumProject.getBacklog()) {
+            iDs.add(""+story.getId());
+        }
+        for (String iD : iDs) {
+            HashMap<String, String> arguments = new HashMap<>();
+            arguments.put("storyID", iD);
+            actionSet.getProjectAction(Operation.DELETE_STORY).execute(scrumProject, arguments);
+        }
+        if(databaseDeleteTeamProjectError(scrumProject.getId())==false) return false;
+        return databaseDeleteProjectError(scrumProject.getId());
+    }
+    
+    private boolean databaseDeleteTeamProjectError(int projectID){
+        String where = "project = "+projectID;
+        return new SqliteInterface().deleteFrom("teams",where);
+    }
+    
+    private boolean databaseDeleteProjectError(int projectID){
+        String where = "id = "+projectID;
+        return new SqliteInterface().deleteFrom("projects",where);
+    }
 }
